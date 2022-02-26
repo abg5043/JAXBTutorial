803f1b666e4d46c4e9f9ed04a49acaa7791c8865
abg5043 <aaron.b.grant@gmail.com>
2022-02-25 19:00:13 -0600

    Changes by christin

---
+++ src/main/java/Credentials_Option3.java
@@ -0,0 +1,39 @@
+/*
+
+// JAKARTA 3.0.0 (using different dependencies at the same time
+// creates errors)
+
+import jakarta.xml.bind.annotation.*;
+
+@XmlRootElement(name = "credentials")
+@XmlAccessorType(XmlAccessType.FIELD)
+
+public class Credentials {
+
+  @XmlElement(name = "user")
+  String user;
+
+  @XmlElement(name = "password")
+  String password;
+
+  @XmlAttribute(name = "xhint")
+  String xhint;
+
+  @XmlElement(name = "host")
+  String host;
+
+  @XmlElement(name = "port")
+  String port;
+
+  @Override
+  public String toString() {
+    return "Credentials {" +
+        "xhint=" + xhint + ", " +
+        "user=" + user + ", " +
+        "password=" + password + ", " +
+        "host=" + host + ", " +
+        "port=" + port + ", " +
+        " }";
+  }
+}
+*/
